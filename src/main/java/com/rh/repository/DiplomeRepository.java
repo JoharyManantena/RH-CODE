37d339fbebe316e4ab57e7230b5f9a3d93c164b3
Johary Manantena <166318905+JoharyManantena@users.noreply.github.com>
2024-11-22 16:54:15 +0300

    reboot

--- src/main/java/com/rh/repository/DiplomeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rh.repository;
-
-import com.rh.model.Diplome;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DiplomeRepository extends JpaRepository<Diplome, Integer> {
-}
