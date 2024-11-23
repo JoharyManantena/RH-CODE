23c2397002f307c10b65f45ab394966cea785c8e
Johary Manantena <166318905+JoharyManantena@users.noreply.github.com>
2024-11-23 16:43:45 +0300

    Update concep.sql

--- base/concep.sql
+++ base/concep.sql
@@ -10,8 +10,6 @@ CREATE TABLE diplome(
    PRIMARY KEY(id_diplome)
 );
 
-inset into diplome value
-
 CREATE TABLE Client(
    idclient INT,
    email VARCHAR(50)  NOT NULL,
@@ -56,22 +54,37 @@ CREATE TABLE cv(
 );
 
 
+DROP TABLE IF EXISTS categorie_personnel;
+CREATE TABLE categorie_personnel (
+   id_categorie INT AUTO_INCREMENT,
+   nom_categorie VARCHAR(50) NOT NULL, -- ouvrier , cadre , ...
+   niveau_hierarchique INT NOT NULL,
+   PRIMARY KEY(id_categorie)
+);
+
+
 -- apres embauche 
-CREATE TABLE personnel(
+CREATE TABLE personnel (
    id_personnel INT AUTO_INCREMENT,
-   nom VARCHAR(255) ,
-   prenom VARCHAR(255) ,
+   nom VARCHAR(255),
+   prenom VARCHAR(255),
    date_naissance DATE,
-   adresse VARCHAR(255) ,
+   adresse VARCHAR(255),
    date_embauche DATE,
-   salaire DECIMAL(15,2)  ,
+   salaire DECIMAL(15,2),
    id_departement INT,
-   poste VARCHAR(50) ,
+   id_categorie INT,
+   poste VARCHAR(50),
    id_cv INT NOT NULL,
    PRIMARY KEY(id_personnel),
    UNIQUE(id_cv),
-   FOREIGN KEY(id_cv) REFERENCES cv(id_cv)
+   FOREIGN KEY(id_cv) REFERENCES cv(id_cv),
+   FOREIGN KEY(id_categorie) REFERENCES categorie_personnel(id_categorie)
 );
+
+
+
+
 
 CREATE TABLE test(
    id_test INT AUTO_INCREMENT,
@@ -101,25 +114,152 @@ CREATE TABLE entretien(
 );
 
 
+CREATE TABLE type_contrat (
+   id_type INT AUTO_INCREMENT,
+   nom VARCHAR(100) NOT NULL,
+   abreviation VARCHAR(50),
+   PRIMARY KEY(id_type)
+);
+
+CREATE TABLE contrat_employe (
+   id_contrat_employe INT AUTO_INCREMENT,
+   date_debut DATE,
+   date_fin DATE,
+   id_personnel INT NOT NULL,
+   id_type INT NOT NULL,
+   PRIMARY KEY(id_contrat_employe),
+   FOREIGN KEY (id_personnel) REFERENCES personnel(id_personnel),
+   FOREIGN KEY (id_type) REFERENCES type_contrat(id_type)
+);
 
 
-CREATE OR REPLACE VIEW voir_filtrage AS
-SELECT 
-    cv.nom, 
-    cv.prenom, 
-    resulat_test.points AS resultat_test, 
-    cv.note AS note_cv,
-    cv.annees_experience, 
-    cv.date_postulation, 
-    (SELECT libelle FROM diplome WHERE diplome.id_diplome = cv.id_diplome) AS diplome
-FROM 
-    resulat_test
-JOIN 
-    test ON test.id_test = resulat_test.id_test
-JOIN 
-    cv ON cv.id_cv = test.id_cv
-ORDER BY 
-    resulat_test.points DESC;
 
 
-select * from voir_filtrage ;
+----    Partie 2  --------------------------------------------
+--------------------------------------------------------------
+
+
+-- algorithme de triage lies a cv (filtrer les CV)
+CREATE TABLE ats (
+   id_ats INT AUTO_INCREMENT,
+   id_cv INTEGER NOT NULL,
+   statut VARCHAR(50) NOT NULL,
+   date_statut DATE NOT NULL,
+   notifications_sent BOOLEAN DEFAULT FALSE,
+   PRIMARY KEY(id_ats),
+   FOREIGN KEY(id_cv) REFERENCES cv(id_cv)
+);
+ALTER TABLE ats ADD COLUMN score INT;
+
+
+CREATE TABLE evaluation_interactive (
+   id_evaluation INT AUTO_INCREMENT,
+   type_evaluation VARCHAR(50) NOT NULL,
+   score NUMERIC(5,2),
+   id_test INTEGER NOT NULL,
+   PRIMARY KEY(id_evaluation),
+   FOREIGN KEY(id_test) REFERENCES test(id_test)
+);
+
+
+-- b. Intelligence Artificielle et Machine Learning
+--  Prédiction de la Réussite : Algorithmes analysant les données des candidats pour
+-- prédire leur adéquation avec la culture d'entreprise et leur potentiel de performance.
+--  Chatbots de Recrutement : Utilisation de chatbots pour interagir avec les candidats,
+-- répondre à leurs questions et les guider tout au long du processus de recrutement. 
+
+
+
+----    Partie 3  --------------------------------------------
+--------------------------------------------------------------
+
+
+--  lies a contrat employer 
+CREATE TABLE rupture_contrat (
+    id_rupture INT AUTO_INCREMENT,
+    id_contrat_employe INT NOT NULL,
+    type_rupture VARCHAR(50) NOT NULL, -- Démission, licenciement, etc.
+    date_notification DATE NOT NULL,
+    indemnites DECIMAL(15,2),
+    PRIMARY KEY(id_rupture),
+    FOREIGN KEY (id_contrat_employe) REFERENCES contrat_employe(id_contrat_employe)
+);
+
+CREATE TABLE document_fin (
+    id_document INT AUTO_INCREMENT,
+    id_rupture INT NOT NULL,
+    type_document VARCHAR(50) NOT NULL, -- Certificat de travail, etc.
+    date_emission DATE NOT NULL,
+    PRIMARY KEY(id_document),
+    FOREIGN KEY (id_rupture) REFERENCES rupture_contrat(id_rupture)
+);
+
+
+
+
+
+
+
+
+CREATE TABLE type_conge (
+    id_type_conge INT AUTO_INCREMENT, -- Specify the data type before AUTO_INCREMENT
+    nom_conge VARCHAR(50) NOT NULL,  -- Congé payé, maladie, etc.
+    PRIMARY KEY (id_type_conge)      -- Specify the column for the primary key
+);
+
+
+CREATE TABLE demande_conge (
+   id_demande INT AUTO_INCREMENT,
+   id_personnel INTEGER NOT NULL,
+   id_type_conge INTEGER NOT NULL,
+   date_demande DATE NOT NULL,
+   date_debut DATE NOT NULL,
+   date_fin DATE,
+   statut VARCHAR(50) DEFAULT 'En attente', -- Approuvé, rejeté, etc.
+   PRIMARY KEY(id_demande),
+   FOREIGN KEY(id_personnel) REFERENCES personnel(id_personnel),
+   FOREIGN KEY(id_type_conge) REFERENCES type_conge(id_type_conge)
+);
+
+
+
+-- heure supplementaire    
+
+CREATE TABLE heures_sup (
+   id_heure_sup INT AUTO_INCREMENT PRIMARY KEY,
+   id_personnel INTEGER NOT NULL,
+   date_heure_sup DATE NOT NULL,
+   nombre_heures NUMERIC(5, 2) NOT NULL,
+   taux_horaire NUMERIC(10, 2) NOT NULL,
+   montant_total NUMERIC(15, 2) NOT NULL,
+   FOREIGN KEY (id_personnel) REFERENCES personnel(id_personnel)
+);
+
+
+-- etat de paie
+
+-- raha ohatra ka hoe hanao avance , ...
+-- ito azo lazaina par defaut car efa misy impot , ... 
+-- Ankoatra ny avance () variable
+
+CREATE TABLE etat_paie (
+   id_paie INT AUTO_INCREMENT PRIMARY KEY,
+   id_personnel INTEGER NOT NULL,
+   mois_annee DATE NOT NULL,
+   salaire_base NUMERIC(15, 2) NOT NULL,
+   indemnites NUMERIC(15, 2),
+   retenues NUMERIC(15, 2),
+   salaire_net NUMERIC(15, 2) NOT NULL,
+   FOREIGN KEY (id_personnel) REFERENCES personnel(id_personnel)
+);
+
+
+
+CREATE TABLE fiche_paie (
+   id_fiche_paie INT AUTO_INCREMENT PRIMARY KEY,
+   id_personnel INTEGER NOT NULL,
+   mois_annee DATE NOT NULL,
+   anciennete_jours INTEGER,
+   net_a_payer NUMERIC(15, 2) NOT NULL,
+   FOREIGN KEY (id_personnel) REFERENCES personnel(id_personnel)
+);
